2a88ccb1db11596d7dd9e7cfb344e6b753f275bc
mrzaid <zaidahmed87861@gmail.com>
2020-06-04 16:52:00 +0500

    chap4

---
+++ chap04/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    khel();
+}
+fn khel() ->String{
+    println!("{:?}",khel);
+    let s = String::from("AJOA KHELEIN");
+    s
+}
---
+++ chap04/target/debug/deps/chap04-484795468ec99eec.d
@@ -0,0 +1,5 @@
+C:\Users\zaid\Desktop\chap04\target\debug\deps\chap04-484795468ec99eec.exe: src\main.rs
+
+C:\Users\zaid\Desktop\chap04\target\debug\deps\chap04-484795468ec99eec.d: src\main.rs
+
+src\main.rs:
